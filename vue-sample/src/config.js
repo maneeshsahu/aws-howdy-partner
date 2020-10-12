8eb237f83c1805c1cc5832440404a6aa0eaaa672
Maneesh Sahu <maneesh@sahu.haus>
2020-10-12 14:24:29 -0700

    Added Vue Sample

---
+++ vue-sample/src/config.js
@@ -0,0 +1,17 @@
+const { CLIENT_ID, ISSUER, OKTA_TESTING_DISABLEHTTPSCHECK } = process.env
+
+export default {
+  oidc: {
+    clientId: CLIENT_ID,
+    issuer: ISSUER,
+    redirectUri: 'http://localhost:8080/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    testing: {
+      disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK
+    }
+  },
+  resourceServer: {
+    messagesUrl: 'http://localhost:8000/api/messages'
+  }
+}
