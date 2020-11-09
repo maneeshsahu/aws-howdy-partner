94b2b23d84ac26b9912a1a5d4d3b37010797fcd3
Maneesh Sahu <maneesh@sahu.haus>
2020-11-08 21:17:32 -0800

    Return content-type
    Updated to application/json

--- aws-serverless-api-gateway-v2/messages.js
+++ aws-serverless-api-gateway-v2/messages.js
@@ -1,24 +1,27 @@
-'use strict';
+"use strict";
 
-module.exports.handler = async event => {
+module.exports.handler = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(
       {
         messages: [
           {
-            date:  new Date(new Date().getTime() - 1000 * 60 * 60),
-            text: 'Howdy Partner!'
-          },      
-          {
-            date:  new Date(),
-            text: 'Okta + AWS = Awesome'
+            date: new Date(new Date().getTime() - 1000 * 60 * 60),
+            text: "Howdy Partner!",
           },
           {
-            date:  new Date(),
-            text: 'Using API Gateway v2. Serverless!!!'
-          }
-        ]
+            date: new Date(),
+            text: "Okta + AWS = Awesome",
+          },
+          {
+            date: new Date(),
+            text: "Using API Gateway v2. Serverless!!!",
+          },
+        ],
       },
       null,
       2
